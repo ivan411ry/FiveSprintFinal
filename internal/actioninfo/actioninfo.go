5b9631294a7c00e2ad96c480fa2d83722c64b4cc
ivan411ry <ivan411.ry@yandex.ru>
2025-07-21 19:52:32 +0300

    SecondWork

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,14 +1,15 @@
 package actioninfo
-import "log"
+import (
+	"log"
+	)
 
 type DataParser interface {
 Parse(datastring string) error
 ActionInfo() (string, error)
 }
 
-func Info(dataset []string, factory func() DataParser) {
+func Info(dataset []string, dp DataParser) {
 for _, data := range dataset {
-	dp := factory()
 	err := dp.Parse(data)
 	if err != nil {
 		log.Printf("parsing error: %v", err)
